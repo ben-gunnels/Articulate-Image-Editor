6ccc657383c43af4049c5bf7f0b85cea76595999
Benjamin Gunnels <bengunnels8@gmail.com>
2025-04-02 20:51:17 -0700

    Adjustment to slider

--- modules/gui/src/widgets.py
+++ modules/gui/src/widgets.py
@@ -6,6 +6,20 @@ __all__ = [
 
 def scale_widget(master):
     scale_widget_frame = tk.Frame(master, width=300, height=30, bg="lightgray")
-    scaler = tk.Scale(scale_widget_frame, from_=0, to=100, orient=tk.HORIZONTAL)
-    scaler.pack()
+    # slider = tk.Scale(scale_widget_frame, from_=0, to=100, orient=tk.HORIZONTAL)
+    slider = tk.Scale(scale_widget_frame,
+        from_=0,
+        to=100,
+        orient=tk.HORIZONTAL,
+        length=80,
+        sliderlength=20,
+        troughcolor="gray",
+        fg="blue",         # label color
+        bg="white",        # background color
+        activebackground="lightblue",
+        highlightthickness=0,
+        width=15           # thickness of the trough
+    )
+    slider.set(50)
+    slider.pack()
     return scale_widget_frame
